172285b8628b48760ebeeacf284dbe85109c480c
Louie-Cipher <louie.spena@outlook.com>
2022-12-30 18:11:29 -0300

    repository name / copy instead move

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-CLIENT_DIR=/home/louie/wifi-hotspot/client
-SERVER_DIR=/home/louie/wifi-hotspot/server
+CLIENT_DIR=/home/louie/phishing-free-wifi/client
+SERVER_DIR=/home/louie/phishing-free-wifi/server
 
 if [ "$1" = "init" ]; then
     cd $CLIENT_DIR
@@ -9,7 +9,7 @@ if [ "$1" = "init" ]; then
     cd $SERVER_DIR
     npm install
     cd ..
-    mv .env.example .env
+    cp .env.example .env
     echo "Installation complete. Please edit the .env file and run the script again."
 elif [ "$1" = "prod" ]; then
     screen -dmS "wifi-client" npm --prefix $CLIENT_DIR run prod &
