693b1c2b936798dba9134fc2464a274a8d5ef8d6
Louie-Cipher <louie.spena@outlook.com>
2023-01-25 18:25:18 -0300

    use absolute path instead relative

--- run.sh
+++ run.sh
@@ -1,20 +1,19 @@
-#!/bin/sh
+#!/bin/bash
 
 CLIENT_DIR=/home/louie/phishing-free-wifi/client
 SERVER_DIR=/home/louie/phishing-free-wifi/server
 
 if [ "$1" = "init" ]; then
     cd $CLIENT_DIR
-    npm install
+    /usr/bin/npm install
     cd $SERVER_DIR
-    npm install
+    /usr/bin/npm install
     cd ..
     cp .env.example .env
-    echo "Installation complete. Please edit the .env file and run the script again."
 elif [ "$1" = "prod" ]; then
-    screen -dmS "wifi-client" npm --prefix $CLIENT_DIR run prod &
-    screen -dmS "wifi-server" npm --prefix $SERVER_DIR run prod
+    /usr/bin/screen -dmS "wifi-client" /usr/bin/npm --prefix $CLIENT_DIR run prod &
+    /usr/bin/screen -dmS "wifi-server" /usr/bin/npm --prefix $SERVER_DIR run prod
 else
-    screen -dmS "wifi-client" npm --prefix $CLIENT_DIR run dev &
-    screen -dmS "wifi-server" npm --prefix $SERVER_DIR run dev
+    /usr/bin/screen -dmS "wifi-client" /usr/bin/npm --prefix $CLIENT_DIR run dev &
+    /usr/bin/screen -dmS "wifi-server" /usr/bin/npm --prefix $SERVER_DIR run dev
 fi
