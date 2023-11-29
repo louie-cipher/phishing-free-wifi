62cb7f57f3f890d84846664fd6a6b4c45eb8fae1
Louie-Cipher <louie.spena@outlook.com>
2023-11-29 20:48:27 -0300

    create build option; open tab on current window

--- run.bat
+++ run.bat
@@ -2,19 +2,25 @@
 set DIR=%~dp0
 
 if "%1" == "init" (
-    copy .env.example .env
-    cd %DIR%\server
-    npm install
-    cd ..\client
-    npm install
+    if not exist "%DIR%\.env" (
+        copy .env.example .env
+    )
+    npm --prefix %DIR%\server install
+    npm --prefix %DIR%\client install
+    exit
+)
+
+if "%1" == "build" (
+    npm --prefix %DIR%\server run build
+    npm --prefix %DIR%\client run build
     exit
 )
 
 if "%1" == "prod" (
-    wt new-tab PowerShell -c "npm --prefix %DIR%\server start"
+    wt -w 0 new-tab PowerShell -c "npm --prefix %DIR%\server start"
     wt -w 0 new-tab PowerShell -c "npm --prefix %DIR%\client start"
     exit
 )
 
-wt new-tab PowerShell -c "npm --prefix %DIR%\server run dev"
+wt -w 0 new-tab PowerShell -c "npm --prefix %DIR%\server run dev"
 wt -w 0 new-tab PowerShell -c "npm --prefix %DIR%\client run dev"
