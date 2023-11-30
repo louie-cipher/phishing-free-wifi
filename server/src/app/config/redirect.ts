8a26dc9dc95108ef4e0c8ab9ea6b6539f02fade2
Louie-Cipher <louie.spena@outlook.com>
2023-11-30 17:11:13 -0300

    do not redirect localhost requests

--- server/src/app/config/redirect.ts
+++ server/src/app/config/redirect.ts
@@ -4,10 +4,12 @@ import { AccessEntry } from 'db/entities/AccessEntry';
 const hostname = 'facebook.com';
 
 export default (app: Express) =>
-	app.use((req, res, next) => {
+	app.use(async (req, res, next) => {
 		if (req.get('host').includes(hostname)) return next();
 
 		const { ip } = req;
+
+		if (['127.0.0.1', '::1', 'localhost', '192.168.4.1'].includes(ip)) return next();
 
 		AccessEntry.findOneBy({ ip })
 			.then((entry) =>
