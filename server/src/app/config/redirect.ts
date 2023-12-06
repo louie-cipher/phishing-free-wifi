3683db45cd62bc75e519d8f42ddd01153a19b010
Louie-Cipher <louie.spena@outlook.com>
2023-12-06 10:28:45 -0300

    use frontend host and port variables

--- server/src/app/config/redirect.ts
+++ server/src/app/config/redirect.ts
@@ -1,21 +1,21 @@
 import { Express } from 'express';
 import { AccessEntry } from '../../db/entities/AccessEntry';
+import { host, port, frontendHost, frontendPort } from '../../';
 
 export default (app: Express) =>
 	app.use(async (req, res, next) => {
-		const reqHost = req.get('host');
-		if (reqHost.includes('facebook.com') || reqHost.includes('192.168.4.1'))
-			return next();
-
 		const { ip } = req;
 
-		if (['127.0.0.1', '::1', 'localhost', '192.168.4.1'].includes(ip)) return next();
+		if (['127.0.0.1', '::1', 'localhost'].includes(ip)) return next();
+		
+		const reqHost = req.get('host');
+		if (reqHost.includes(host) || reqHost.includes(frontendHost)) return next();
 
 		AccessEntry.findOneBy({ ip })
 			.then((entry) =>
 				entry && entry.internetAccess
 					? next()
-					: res.redirect('http://192.168.4.1:3000')
+					: res.redirect(`http://${frontendHost}:${frontendPort}`)
 			)
-			.catch(() => res.redirect('http://192.168.4.1:3000'));
+			.catch(() => res.redirect(`http://${frontendHost}:${frontendPort}`));
 	});
