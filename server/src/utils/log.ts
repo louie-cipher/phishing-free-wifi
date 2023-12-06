5687d5d695cd0331382f049fe025255df706ed27
Louie-Cipher <louie.spena@outlook.com>
2023-12-06 10:07:30 -0300

    use native toLocaleTimeString

--- server/src/utils/log.ts
+++ server/src/utils/log.ts
@@ -2,18 +2,19 @@ import { gray, blue, red } from 'chalk';
 import { Request, Response } from 'express';
 import userAgent from './userAgent';
 
-const d = (n: number) => (n < 10 ? `0${n}` : `${n}`);
-
-function dateToString(): string {
-	const date = new Date();
-	return `${d(date.getHours())}:${d(date.getMinutes())}:${d(date.getSeconds())}`;
-}
+const nowString = () =>
+	new Date().toLocaleTimeString('pt-BR', {
+		timeZone: 'America/Sao_Paulo',
+		hour: '2-digit',
+		minute: '2-digit',
+		second: '2-digit',
+	});
 
 export const consoleLog = (origin: string, ...args: any[]) =>
-	console.log(gray(dateToString()), blue(`[${origin}]`), ...args);
+	console.log(gray(nowString()), blue(`[${origin}]`), ...args);
 
 export const consoleError = (origin: string, ...args: any[]) =>
-	console.log(gray(dateToString()), red(`[${origin}]`), ...args, '\n', '-'.repeat(80));
+	console.log(gray(nowString()), red(`[${origin}]`), ...args, '\n', '-'.repeat(80));
 
 export function unauthorizedLog(req: Request, res: Response): void {
 	const { ip, os, device } = userAgent(req);
