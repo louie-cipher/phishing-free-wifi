d4c6ef4f71cb90f8f0a13629092a7231e03426e6
Louie-Cipher <louie.spena@outlook.com>
2023-12-06 10:28:06 -0300

    use files full path

--- run.sh
+++ run.sh
@@ -2,23 +2,23 @@ CLIENT_DIR=~/phishing-free-wifi/client
 SERVER_DIR=~/phishing-free-wifi/server
 
 #check if nodejs and npm is installed
-if [ ! -x "$(command -v node)" ] || [ ! -x "$(command -v npm)" ]; then
-    echo "Error: nodejs/npm not installed" >&2
-    echo "Please install nodejs and npm first" >&2
-    echo "curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash" >&2
+if [ ! -x "$(command -v /usr/bin/node)" ] || [ ! -x "$(command -v /usr/bin/npm)" ]; then
+    echo "Error: nodejs/npm not installed"
+    echo "Please install nodejs and npm first"
+    echo "curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash"
     exit 1
 fi
 
 # check if screen is installed
-if ! [ -x "$(command -v screen)" ]; then
-    echo "Error: screen is not installed" >&2
-    echo "sudo apt install screen" >&2
+if ! [ -x "$(command -v /usr/bin/screen)" ]; then
+    echo "Error: screen is not installed"
+    echo "sudo apt install screen"
     exit 1
 fi
 
 if [ "$1" = "install" ]; then
-    npm --prefix $CLIENT_DIR install
-    npm --prefix $SERVER_DIR install
+    /usr/bin/npm --prefix $CLIENT_DIR install
+    /usr/bin/npm --prefix $SERVER_DIR install
     if [ -f .env ]; then
         echo ".env already exists. Skip copying"
     else
@@ -26,19 +26,19 @@ if [ "$1" = "install" ]; then
         echo "edit .env file"
     fi
 elif [ "$1" = "build" ]; then
-    npm --prefix $CLIENT_DIR run build
-    npm --prefix $SERVER_DIR run build
+    /usr/bin/npm --prefix $CLIENT_DIR run build
+    /usr/bin/npm --prefix $SERVER_DIR run build
 elif [ "$1" = "dev" ]; then
-    screen -dmS "wifi-client" npm --prefix $CLIENT_DIR run dev &
-    screen -dmS "wifi-server" npm --prefix $SERVER_DIR run dev
+    /usr/bin/screen -dmS "wifi-client" /usr/bin/npm --prefix $CLIENT_DIR run dev &
+    /usr/bin/screen -dmS "wifi-server" /usr/bin/npm --prefix $SERVER_DIR run dev
 else
-    #check if server/node_modules and client/node_modules exists
-    if [ ! -d "$CLIENT_DIR/node_modules" ] || [ ! -d "$SERVER_DIR/node_modules" ]; then
+    #check if server//usr/bin/node_modules and client//usr/bin/node_modules exists
+    if [ ! -d "$CLIENT_DIR/node_modules" ] || [ ! -d "$SERVER_DIR//node_modules" ]; then
         read -p "No node_modules found. Install now? [y/n] " -n 1 -r
         echo
         if [[ $REPLY =~ ^[Yy]$ ]]; then
-            npm --prefix $CLIENT_DIR i
-            npm --prefix $SERVER_DIR i
+            /usr/bin/npm --prefix $CLIENT_DIR i
+            /usr/bin/npm --prefix $SERVER_DIR i
             echo "modules installed. Proceeding"
         else
             echo "Please run './run.sh install' first"
@@ -51,16 +51,16 @@ else
         read -p "No dist found. Compile now? [y/n] " -n 1 -r
         echo
         if [[ $REPLY =~ ^[Yy]$ ]]; then
-            npm --prefix $CLIENT_DIR run build
-            npm --prefix $SERVER_DIR run build
+            /usr/bin/npm --prefix $CLIENT_DIR run build
+            /usr/bin/npm --prefix $SERVER_DIR run build
             echo "dist compiled. Proceeding"
         else
             echo "Please run './run.sh build' first"
             exit 1
         fi
     fi
-    screen -dmS "wifi-client" npm --prefix $CLIENT_DIR start &
-    screen -dmS "wifi-server" npm --prefix $SERVER_DIR start
+    /usr/bin/screen -dmS "wifi-client" /usr/bin/npm --prefix $CLIENT_DIR start &
+    /usr/bin/screen -dmS "wifi-server" /usr/bin/npm --prefix $SERVER_DIR start
 
     echo "run 'screen -r wifi-client' to see client logs"
     echo "run 'screen -r wifi-server' to see server logs"
