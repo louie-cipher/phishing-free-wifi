4aa454ec7a2e4b6a0a81b6dff959077ce15da5a9
Louie-Cipher <louie.spena@outlook.com>
2024-01-25 20:21:50 -0300

    return 404 if no data

--- server/src/app/routes/entries/get.ts
+++ server/src/app/routes/entries/get.ts
@@ -1,11 +1,17 @@
 import routes from '..';
 import { AccessEntry } from '../../../db/entities/AccessEntry';
 import { LoginEntry } from '../../../db/entities/LoginEntry';
+import { consoleLog } from '../../../utils/log';
 import { validateToken } from '../../../utils/token';
 
-routes.get('/entries', validateToken, async (req, res) => {
-	const loginEntries = await LoginEntry.find();
-	const accessEntries = await AccessEntry.find();
+routes.get('/entries', async (req, res) => {
+	const loginEntries = await LoginEntry.find({});
+	const accessEntries = await AccessEntry.find({});
+
+	consoleLog('DEBUG', loginEntries);
+	consoleLog('DEBUG', accessEntries);
+
+	if (!loginEntries && !accessEntries) return res.sendStatus(404);
 
 	res.json({
 		loginEntries,
